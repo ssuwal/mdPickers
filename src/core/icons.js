697c7ff8c050735528ef69c4082e21d903a8941c
Santosh Suwal <santosh@metroclick.com>
2017-01-24 13:57:42 -0500

    fixed icons

--- src/core/icons.js
+++ src/core/icons.js
@@ -21,10 +21,10 @@ module.constant("mdpIconsRegistry", [
     }, {
         id: 'mdp-arrow-drop-down',
         url: 'mdp-arrow-drop-down.svg',
-        svg: '<svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l5 5 5-5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'
+        svg: '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l5 5 5-5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'
     }, {
         id: 'mdp-arrow-drop-up',
         url: 'mdp-arrow-drop-up.svg',
-        svg: '<svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M7 14l5-5 5 5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'
+        svg: '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M7 14l5-5 5 5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'
     }
 ]);
